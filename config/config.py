643e513592eab509b3f5a7b3eb4c335273ba6a8a
ximmv <ibaydullayevyodgorbek46@gmail.com>
2026-06-29 02:14:34 +0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,13 @@
 
+from aiogram import Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config.settings import BOT_TOKEN
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    )
+)
