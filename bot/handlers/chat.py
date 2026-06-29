b28b9c74d05c20ea0efc693b868188d9e3508492
ximmv <ibaydullayevyodgorbek46@gmail.com>
2026-06-29 20:58:37 +0500

    Update chat.py

--- bot/handlers/chat.py
+++ bot/handlers/chat.py
@@ -1,20 +1,7 @@
 from aiogram import Router
+from aiogram.enums import ChatAction
 from aiogram.types import Message
 
 from bot.ai.gemini import generate_text
 
 router = Router()
-
-
-@router.message()
-async def chat_handler(message: Message):
-    text = message.text
-
-    if not text:
-        return
-
-    wait = await message.answer("✍️ Yozmoqda...")
-
-    response = await generate_text(text)
-
-    await wait.edit_text(response)
