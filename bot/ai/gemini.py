9808689a5c27a0fe763a49274c75ef9669f692a0
ximmv <ibaydullayevyodgorbek46@gmail.com>
2026-07-06 01:07:12 +0500

    Update gemini.py

--- bot/ai/gemini.py
+++ bot/ai/gemini.py
@@ -5,30 +5,31 @@ from database import get_history, save_message
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
+
 async def generate_text(user_id: int, prompt: str) -> str:
-try:
-history = get_history(user_id)
+    try:
+        history = get_history(user_id)
 
-    contents = []
+        contents = []
 
-    for msg in history:
-        contents.append(
-            f"{msg['role']}: {msg['text']}"
+        for msg in history:
+            contents.append(
+                f"{msg['role']}: {msg['text']}"
+            )
+
+        contents.append(f"user: {prompt}")
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents="\n".join(contents),
         )
 
-    contents.append(f"user: {prompt}")
+        answer = response.text or "❌ Javob olinmadi."
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="\n".join(contents),
-    )
+        save_message(user_id, "user", prompt)
+        save_message(user_id, "assistant", answer)
 
-    answer = response.text or "❌ Javob olinmadi."
+        return answer
 
-    save_message(user_id, "user", prompt)
-    save_message(user_id, "assistant", answer)
-
-    return answer
-
-except Exception as e:
-    return f"❌ Xatolik:\n{e}"
+    except Exception as e:
+        return f"❌ Xatolik:\n{e}"
